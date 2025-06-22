c09d154892d3e8a54ab11005beb35cc685f6eabc
DarkwingDelphi <109265941+DarkwingDelphi@users.noreply.github.com>
2025-06-22 11:14:31 -0500

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,70 +1,40 @@
-
 let player;
 let doors = [];
 let score = 0;
-let gameState = "START";
-let backgroundStage = 0;
+let gameState = "PLAY";
 let bgColor;
-let lastBgChange = 0;
+let lastColorChange = 0;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   player = new Player();
-  resetGame();
-}
-
-function resetGame() {
-  doors = [];
-  score = 0;
-  backgroundStage = 0;
   bgColor = color(0);
-  lastBgChange = 0;
-  player.reset();
-  gameState = "PLAY";
+  for (let i = 0; i < 5; i++) {
+    doors.push(new Door(random(width), -i * 150));
+  }
 }
 
 function draw() {
-  if (gameState === "START") {
-    background(0);
-    fill(255);
-    textSize(32);
-    textAlign(CENTER);
-    text("Don't Touch the Door", width / 2, height / 2);
-    textSize(16);
-    text("Tap to start", width / 2, height / 2 + 30);
-  } else if (gameState === "PLAY") {
+  background(bgColor);
+  if (gameState === "PLAY") {
     drawGame();
   } else if (gameState === "END") {
-    background(0);
-    fill(255);
-    textSize(32);
-    textAlign(CENTER);
-    text("Game Over", width / 2, height / 2);
-    textSize(16);
-    text("Tap to restart", width / 2, height / 2 + 30);
+    drawGameOver();
   }
 }
 
 function drawGame() {
-  background(bgColor);
   score += 0.1;
-
-  if (score - lastBgChange >= 100) {
-    backgroundStage++;
-    bgColor = color(random(255), random(255), random(255));
-    lastBgChange = score;
-  }
-
   fill(255);
-  textSize(14);
+  textSize(16);
   textAlign(LEFT);
   text("Score: " + int(score) + " miles", 10, 20);
 
   player.update();
   player.show();
 
-  if (frameCount % 60 === 0) {
-    doors.push(new Door());
+  if (frameCount % 30 === 0) {
+    doors.push(new Door(random(width), 0));
   }
 
   for (let i = doors.length - 1; i >= 0; i--) {
@@ -79,42 +49,40 @@ function drawGame() {
       doors.splice(i, 1);
     }
   }
+
+  updateBackgroundColor();
 }
 
-function touchStarted() {
-  if (gameState === "START" || gameState === "END") {
-    resetGame();
-  } else {
-    player.jump();
+function updateBackgroundColor() {
+  if (score - lastColorChange >= 10) {
+    bgColor = color(random(255), random(255), random(255));
+    lastColorChange = score;
   }
+}
+
+function drawGameOver() {
+  fill(255);
+  textSize(48);
+  textAlign(CENTER, CENTER);
+  text("Game Over", width / 2, height / 2);
+}
+
+function touchMoved() {
+  player.setX(mouseX);
+  return false;
 }
 
 class Player {
   constructor() {
-    this.size = 40;
-    this.reset();
-  }
-
-  reset() {
+    this.size = 20;
     this.x = width / 2;
     this.y = height - this.size * 2;
-    this.vy = 0;
-    this.gravity = 1.2;
-    this.lift = -20;
   }
 
-  jump() {
-    this.vy += this.lift;
-  }
+  update() {}
 
-  update() {
-    this.vy += this.gravity;
-    this.y += this.vy;
-
-    if (this.y > height - this.size) {
-      this.y = height - this.size;
-      this.vy = 0;
-    }
+  setX(val) {
+    this.x = constrain(val, 0, width - this.size);
   }
 
   show() {
@@ -124,11 +92,12 @@ class Player {
 }
 
 class Door {
-  constructor() {
-    this.x = random(width);
-    this.y = -20;
-    this.size = 30;
-    this.speed = 4;
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.speed = 3;
+    this.size = 20;
+    this.color = color(random(255), random(255), random(255));
   }
 
   update() {
@@ -136,17 +105,22 @@ class Door {
   }
 
   show() {
-    fill(random(255), random(255), random(255));
+    fill(this.color);
     textSize(16);
     textAlign(CENTER);
     text("d00r", this.x, this.y);
   }
 
-  hits(player) {
-    return collideRectCircle(player.x, player.y, player.size, player.size, this.x, this.y, this.size);
+  offscreen() {
+    return this.y > height + 20;
   }
 
-  offscreen() {
-    return this.y > height;
+  hits(player) {
+    return (
+      this.y + this.size > player.y &&
+      this.y < player.y + player.size &&
+      this.x > player.x &&
+      this.x < player.x + player.size
+    );
   }
 }
